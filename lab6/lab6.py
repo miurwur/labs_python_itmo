f59e21bf09aa3a40bb6c7441b59e930a9ec29878
anna barykina <nyabarykina@yandex.ru>
2025-10-16 23:46:54 +0300

    documentation

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -5,23 +5,38 @@ import timeit
 import matplotlib.pyplot as plt
 import random
 
+'''
+    bin_rec: рекурсивная реализация дерева
+    bin_non_rec: итеративная реализация дерева
+    timeit: для измерения времени выполнения
+    matplotlib.pyplot: для построения графиков
+    '''
 
-def benchmark(func, n, repeat=5): # это дописать, верхнюю делитнуть
+def benchmark(func, n, repeat=5):
     """Возвращает среднее время выполнения func(n)"""
     times = timeit.repeat(lambda: func(n), number=1, repeat=repeat)
     return min(times)
+    '''
+       Выполняет функцию func и возвращает минимальное среднее время выполнения.
+       Параметры:
+       func: функция, которую необходимо протестировать
+       n: размер данных
+       repeat: количество повторений замера
+      '''
 
 def main():
-    # фиксированный набор данных
+    # Фиксированный набор данных
     random.seed(2)
+    # Создание тестовых данных: числа с шагом 2
     test_data = list(range(2, 20, 2))
 
+    # Списки для хранения результатов
     res_recursive = []
     res_iterative = []
 
     for n in test_data:
-      res_recursive.append(benchmark(build_tree_recursive,n))
-      res_iterative.append(benchmark(build_tree_iterative,n))
+      res_recursive.append(benchmark(build_tree_recursive,n)) # Замеры времени для рекурсивной генерации
+      res_iterative.append(benchmark(build_tree_iterative,n)) # Замеры времени для итеративной генерации
 
     # Визуализация
     plt.plot(test_data, res_recursive, label="Рекурсивный")
